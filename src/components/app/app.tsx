8e8232062a5c4d00610afd7ae5717cdc67f78273
Petr Saparov <saparov.petr@yandex.ru>
2024-05-13 15:31:08 +0300

    routes added

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,35 @@
-import { ConstructorPage } from '@pages';
+/* eslint-disable arrow-body-style */
+/* eslint-disable prettier/prettier */
+
+import { Route, Routes } from 'react-router-dom';
 import '../../index.css';
 import styles from './app.module.css';
 
 import { AppHeader } from '@components';
+import { ConstructorPage, Feed, ForgotPassword, Login, NotFound404, Profile, ProfileOrders, Register, ResetPassword } from '@pages';
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
-);
+const App = () => {
+
+  return (
+    <>
+      <AppHeader />
+      <Routes>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+        <Route path='*' element={<NotFound404 />} />
+      //  {/* защитить: */}
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/profile/orders' element={<ProfileOrders />} />
+      </Routes>
+    </>
+  )
+}
 
 export default App;
+
+
+//  {/* <Route path='/' element={<ProtectedRoute accessRoles={[ROLE.USER, ROLE.ADMIN]} ><Home /></ProtectedRoute>} /> */}
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,27 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 import App from './components/app/app';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import store from './services/store';
 
 const container = document.getElementById('root') as HTMLElement;
-const root = ReactDOMClient.createRoot(container!);
+// const root = ReactDOMClient.createRoot(container!);
 
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+const root = createRoot(container);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <Provider store={store} children={undefined}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	// </Provider>
 );
